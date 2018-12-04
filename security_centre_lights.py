839e51e457d95f862e87f8a94091d87ebc890ee9
Sarah Townson <sarah.townson@scienceoxford.com>
2018-12-04 10:56:07 +0000

    Create security_centre_lights.py

---
+++ security_centre_lights.py
@@ -0,0 +1,25 @@
+from microbit import *
+import neopixel
+import radio
+
+np = neopixel.NeoPixel(pin2, 18)
+
+radio.on()
+
+while True:
+    incoming = radio.receive()
+    while incoming is None:
+        incoming = radio.receive()
+        for pixel_id in range(0, len(np)):
+            np[pixel_id] = (0, 60, 0)
+        np.show()
+        display.show(Image.XMAS)
+        sleep(100)
+    display.show(Image.ANGRY)
+    for i in range(0, 10):
+        for pixel_id in range(0, len(np)):
+            np[pixel_id] = (60, 0, 0)
+        np.show()
+        sleep(100)
+        np.clear()
+        sleep(100)
